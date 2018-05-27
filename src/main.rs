989c85cc42096da0c2dd763bb7e796cc5680a55b
Tamir Bahar <tamir@north-bit.com>
2018-05-27 21:33:53 +0300

    Check uniqueness based on regex. Parts outside the regex capture do not count for uniqueness. This is for filtering for specific fields or dropping specific fields.
    Future work should include the opposite - exclude regex.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 extern crate clap;
+
 use clap::{App, Arg};
 
 extern crate fxhash;
+
 use fxhash::FxHashSet;
+
+extern crate regex;
+
+use regex::bytes::Regex;
+
+extern crate itertools;
+
+use itertools::Itertools;
 
 use std::collections::VecDeque;
 use std::io::{BufRead, StdinLock, Write};
@@ -73,18 +83,25 @@ fn main() {
     let matches = App::new("uq (lostutils)")
         .arg(
             Arg::with_name("capacity")
-                .short("n")
+                .long("capacity")
                 .help("Number of unique entries to remember.")
                 .value_name("capacity")
                 .takes_value(true),
         )
         .arg(
             Arg::with_name("override")
-                .short("r")
+                .long("override")
                 .help("Override old unique entries when capacity reached.\nWhen not used, uq will die when the capacity is exceeded.")
                 .requires("capacity")
                 .value_name("override")
                 .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("include")
+                .long("include")
+                .help("Regex capture to use for matching")
+                .value_name("include")
+                .takes_value(true),
         )
         .get_matches();
 
@@ -102,13 +119,34 @@ fn main() {
         _ => unique_filter(),
     };
 
+
     let (_in, _out) = (std::io::stdin(), std::io::stdout());
     let (input, mut output) = (_in.lock(), _out.lock());
 
     let mut stdin_reader = StdinReader::new(input);
-    while let Some(line) = stdin_reader.next_line() {
-        if unique_filter(line) {
-            output.write_all(line).expect("Failed writing line");
+    if let Some(include) = Some(r"(\d+)") {
+//    if let Some(include) = matches.value_of("include") {
+        let re = Regex::new(include.clone()).unwrap();
+        while let Some(line) = stdin_reader.next_line() {
+            let mut x: Vec<u8> = Vec::new();
+            for match_str in re.captures(line).unwrap().iter().dropping(1).filter_map(|opt_match| match opt_match {
+                Some(m) => Some(m.as_bytes()),
+                None => None,
+            }) {
+
+            println!("match_str: {:?}", &match_str);
+                x.extend(match_str);
+            }
+            println!("Bla: {:?}", &x);
+            if unique_filter(&x) {
+                output.write_all(line).expect("Failed writing line");
+            }
+        }
+    } else {
+        while let Some(line) = stdin_reader.next_line() {
+            if unique_filter(line) {
+                output.write_all(line).expect("Failed writing line");
+            }
         }
     }
 }
